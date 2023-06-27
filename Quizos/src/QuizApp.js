7fcccbdd2589d5abf4260ba14e43f61db4fafaea
Jyotendra Singh Chuahan <tezosgecoffcial@gmail.com>
2023-06-27 22:32:31 +0530

    ui finalized v1.2

--- Quizos/src/QuizApp.js
+++ Quizos/src/QuizApp.js
@@ -1,27 +1,84 @@
-import React, { useState } from 'react';
-import './QuizApp.css';
+import React, { useState } from "react";
+import "./QuizApp.css";
 
 const QuizApp = () => {
   const [questions] = useState([
     {
-      question: 'What is the capital of France?',
-      options: ['Paris', 'London', 'Berlin', 'Rome'],
-      answer: 'Paris'
+      question: "What is the native cryptocurrency of the Tezos blockchain?",
+      options: ["Bitcoin", "Ethereum", "Tez", "Cardano"],
+      answer: "Tez",
     },
     {
-      question: 'Which planet is known as the Red Planet?',
-      options: ['Venus', 'Mars', 'Jupiter', 'Saturn'],
-      answer: 'Mars'
+      question: "Which consensus algorithm does Tezos use?",
+      options: [
+        "Proof of Stake (PoS)",
+        "Proof of Work (PoW)",
+        "Delegated Proof of Stake (DPoS)",
+        "Proof of Authority (PoA)",
+      ],
+      answer: "Proof of Stake (PoS)",
     },
     {
-      question: 'What is the chemical symbol for gold?',
-      options: ['Ag', 'Au', 'Cu', 'Pt'],
-      answer: 'Au'
-    }
+      question: "What is the primary purpose of the Tezos blockchain?",
+      options: [
+        "Decentralized finance (DeFi)",
+        "Smart contracts",
+        "Identity verification",
+        "Supply chain management",
+      ],
+      answer: "Smart contracts",
+    },
+    {
+      question: "Who is the founder of Tezos?",
+      options: [
+        "Vitalik Buterin",
+        "Satoshi Nakamoto",
+        "Charles Hoskinson",
+        "Arthur Breitman",
+      ],
+      answer: "Arthur Breitman",
+    },
+    {
+      question:
+        "Which programming language is primarily used for writing smart contracts on Tezos?",
+      options: ["Solidity", "JavaScript", "Michelson", "Rust"],
+      answer: "Michelson",
+    },
+    {
+      question: 'What is the concept of "self-amendment" in Tezos?',
+      options: [
+        "The ability to automatically upgrade the blockchain protocol",
+        "The ability to reverse transactions",
+        "The ability to print new coins",
+        "The ability to fork the blockchain",
+      ],
+      answer: "The ability to automatically upgrade the blockchain protocol",
+    },
+    {
+      question: "What is the total supply of Tezos (XTZ) tokens?",
+      options: ["10 million", "100 million", "1 billion", "10 billion"],
+      answer: "1 billion",
+    },
+    {
+      question: "Which smart contract standard is used on Tezos?",
+      options: ["ERC-20", "NEP-5", "TZIP-12", "BEP-20"],
+      answer: "TZIP-12",
+    },
+    {
+      question:
+        "What is the name of the on-chain governance mechanism in Tezos?",
+      options: ["DAO", "DPOS", "Liquid Democracy", "On-chain Governance"],
+      answer: "On-chain Governance",
+    },
+    {
+      question: "Which year was the Tezos blockchain launched?",
+      options: ["2016", "2017", "2018", "2019"],
+      answer: "2018",
+    },
   ]);
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [selectedOption, setSelectedOption] = useState('');
+  const [selectedOption, setSelectedOption] = useState("");
   const [correctAnswers, setCorrectAnswers] = useState(0);
 
   const handleOptionSelect = (option) => {
@@ -32,7 +89,7 @@ const QuizApp = () => {
     if (selectedOption === questions[currentQuestion].answer) {
       setCorrectAnswers(correctAnswers + 1);
     }
-    setSelectedOption('');
+    setSelectedOption("");
     setCurrentQuestion(currentQuestion + 1);
   };
 
@@ -57,7 +114,10 @@ const QuizApp = () => {
       return (
         <div className="quiz-container">
           <h2 className="quiz-title">Quiz completed!</h2>
-          <p className="quiz-result">You answered {correctAnswers} out of {questions.length} questions correctly.</p>
+          <p className="quiz-result">
+            You answered {correctAnswers} out of {questions.length} questions
+            correctly.
+          </p>
         </div>
       );
     }
@@ -67,7 +127,11 @@ const QuizApp = () => {
         <h2 className="quiz-title">Question {currentQuestion + 1}</h2>
         <p className="quiz-question">{questions[currentQuestion].question}</p>
         {renderOptions()}
-        <button className="quiz-next-btn" onClick={handleNextQuestion} disabled={!selectedOption}>
+        <button
+          className="quiz-next-btn"
+          onClick={handleNextQuestion}
+          disabled={!selectedOption}
+        >
           Next Question
         </button>
       </div>
