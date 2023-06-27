fca6dc17cdb2ffb61509481a5785666e0c341f2a
Shreyas <shreyassingh621@gmail.com>
2023-06-27 23:25:04 +0530

    Structure changes V2

--- Quizos/src/index.js
+++ Quizos/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import QuizApp from './components/QuizApp';
+import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <QuizApp />
+    <App></App>
   </React.StrictMode>,
   document.getElementById('root')
 );
