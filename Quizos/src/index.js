8a32c6bfbfc6c757fabeb9451a0fae4d2abd3ab9
Jyotendra Singh Chuahan <tezosgecoffcial@gmail.com>
2023-06-27 23:26:36 +0530

    nothing

--- Quizos/src/index.js
+++ Quizos/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import QuizApp from './components/QuizApp';
 
+
 ReactDOM.render(
   <React.StrictMode>
+   
     <QuizApp />
   </React.StrictMode>,
   document.getElementById('root')
