ef3d9dad2664eba590d81b9b5c36d9d79cac4c21
Jyotendra Singh Chuahan <tezosgecoffcial@gmail.com>
2023-06-28 10:31:37 +0530

    tacquito and beacon installed with login ui

--- Quizos/src/components/Login.jsx
+++ Quizos/src/components/Login.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/login.css'; // Import the CSS file from the style folder
 
 const Login = () => {
-
   return (
-    <div>
-        <Link to='/quiz'><button>Login with Temple Wallet</button></Link> 
+    <div className="login-container">
+      <h1>Welcome to Quizos</h1>
+      <p>Please login to proceed.</p>
+      <Link to="/quiz" className="login-button">
+        Login with Temple Wallet
+      </Link>
     </div>
   );
 };
