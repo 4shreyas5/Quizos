df60ac4be2db22f8a835fd5f4b257281947a7ecc
Akansha <akanshabharti1213@gmail.com>
2023-06-28 15:57:26 +0530

    changes in contract

--- Quizos/contract/certificate.py
+++ Quizos/contract/certificate.py
@@ -7,14 +7,14 @@ class Certificate(sp.Contract):
     @sp.entry_point
     def mintCertificate(self, params):
         certificate = {
-            "name": params.name,
+            "userid": params.userid,
             "score": params.score
         } 
 
         token_id = sp.sha256(sp.pack(certificate))
         self.data.storedCertificates[token_id] = certificate
 
-@sp.add_test(name="Mint Certificate Test")
+@sp.add_test(userid="Mint Certificate Test")
 def test_mint_certificate():
     scenario = sp.test_scenario()
 
@@ -22,17 +22,17 @@ def test_mint_certificate():
     contract = Certificate()
 
     # Define test parameters
-    name = 12345  # Change the name parameter to sp.TIntOrNat or sp.TNat type
+    userid = 12345  # Change the userid parameter to sp.TIntOrNat or sp.TNat type
     score = 95
 
     # Create test account and call the mintCertificate entry point
     scenario.h1("Mint Certificate")
     scenario += contract
-    scenario += contract.mintCertificate(name=name, score=score).run(sender=sp.test_account)
+    scenario += contract.mintCertificate(userid=userid, score=score).run(sender=sp.test_account)
 
     # Check if certificate was minted successfully
     scenario.h2("Verify Certificate") 
     scenario.verify(len(contract.data.storedCertificates) == 1)
-    scenario.verify(list(contract.data.storedCertificates.keys())[0] == sp.sha256(sp.pack({"name": name, "score": score})))
-    scenario.verify(contract.data.storedCertificates[list(contract.data.storedCertificates.keys())[0]]["name"] == name)
+    scenario.verify(list(contract.data.storedCertificates.keys())[0] == sp.sha256(sp.pack({"userid": userid, "score": score})))
+    scenario.verify(contract.data.storedCertificates[list(contract.data.storedCertificates.keys())[0]]["userid"] == userid)
     scenario.verify(contract.data.storedCertificates[list(contract.data.storedCertificates.keys())[0]]["score"] == score)
